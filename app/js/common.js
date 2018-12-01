499cccb1952b55eb10ee4a7b9305f65a7c2dd934
andreysobal <say6287@gmail.com>
2018-12-01 22:00:50 +0300

    1. Fixed trouble with text width in section_4. 2. Made up footer.

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,10 @@
-$(function() {
-	console.log("loaded!");
-	// Custom JS
-
-});
+console.log("loaded!");
+// smooth scroll to the anchor id
+function scrollToTop() {
+	var top = $('#top').offset().top;
+	setTimeout(function() {
+		$('html, body').animate({
+			scrollTop: top
+		}, 1500);
+	}, 10);
+}
